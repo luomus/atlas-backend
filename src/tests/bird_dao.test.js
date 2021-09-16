310b547bc2dee999a13cb9eb2ccbbeb11d4ac86f
Elma Nevala <elma.nevala@helsinki.fi>
2021-09-16 19:02:50 +0300

    broke test, to see if actions work

--- src/tests/bird_dao.test.js
+++ src/tests/bird_dao.test.js
@@ -1,5 +1,5 @@
 const bird_dao = require('../main/dao/bird_dao.js');
 
 test('retuns a bird', () => {
-    expect(bird_dao.getBirds()).toEqual(expect.anything());
+    expect(bird_dao.getBirds()).toEqual(expec.anything());
 });
