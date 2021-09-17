fbd47d9cc27b7a7342fbc8e672f519ef8901184f
juliapalorinne <julia.palorinne@helsinki.fi>
2021-09-17 10:00:27 +0300

    database first version update

--- src/main/dao/bird_dao.js
+++ src/main/dao/bird_dao.js
@@ -29,17 +29,17 @@ class bird_dao {
 
     get(sql, params = []) {
         return new Promise((resolve, reject) => {
-           this.db.get(sql, params, (err, result) => {
-             if (err) {
-               console.log('Error running sql: ' + sql)
-               console.log(err)
-               reject(err)
-            } else {
-               resolve(result)
-            }
-          })
+            this.db.get(sql, params, (err, result) => {
+                if (err) {
+                    console.log('Error running sql: ' + sql)
+                    console.log(err)
+                    reject(err)
+                } else {
+                    resolve(result)
+                }
+            })
         })
-      }
+    }
     
     all(sql, params = []) {
         return new Promise((resolve, reject) => {
