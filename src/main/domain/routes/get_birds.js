e2dce13c94ea37f37d5ea41805603b8eb65df4d0
Emma Valtonen <emma.valtonen@helsinki.fi>
2021-09-17 13:43:20 +0300

    Fix get_birds.js

--- src/main/domain/routes/get_birds.js
+++ src/main/domain/routes/get_birds.js
@@ -2,11 +2,14 @@ const Promise = require('bluebird')
 const birdDao = require(__rootdir + "/dao/bird_dao.js")
 const bird_table = require(__rootdir + '/dao/species_table.js')
 
-function getBirds (req, res) {
+async function getBirds (req, res) {
     const dao = new birdDao(__rootdir + '/birds.db')
     const table = new bird_table(dao)
-
-    res.send(table.getById(1))
+    table.getById(1).then(function(fromResolve) {
+        res.send(fromResolve)
+    }, function(fromReject) {
+        res.send(null)
+    })
 }
 
 module.exports = getBirds
