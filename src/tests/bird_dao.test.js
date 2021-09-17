62b8c68706f7a3b5981a1ce2fee87c8ce79281fb
Elma Nevala <elma.nevala@helsinki.fi>
2021-09-17 16:55:42 +0300

    map test

--- src/tests/bird_dao.test.js
+++
@@ -1,5 +0,0 @@
-const bird_dao = require('../main/dao/bird_dao.js');
-
-test('retuns a bird', () => {
-    expect(bird_dao.getBirds()).toEqual(expect.anything());
-});
---
+++ src/tests/map_service.test.js
@@ -0,0 +1,6 @@
+
+const MapService = require('../main/domain/map_service.js');
+
+test('retuns a bird', () => {
+    expect(MapService).toEqual(expect.anything());
+});
