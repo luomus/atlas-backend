c4046408596db82ba7029c3dba7fc2e4e58a54f9
Emma Valtonen <emma.valtonen@helsinki.fi>
2021-09-29 14:16:00 +0300

    Create bird_dao mock

---
+++ src/main/dao/__mocks__/bird_dao.js
@@ -0,0 +1,56 @@
+function BirdDao() {
+    const data = [
+        {
+            "mxCode":25836,
+            "speciesFI":"GAVSTE",
+            "speciesEN":"Gavia stellata",
+            "speciesSCI":"Kaakkuri",
+            "speciesAbbr":"Smålom",
+            "speciesGroup_id":null,
+            "visibility":1
+        },
+        {
+            "mxCode":25837,
+            "speciesFI":"GAVARC",
+            "speciesEN":"Gavia arctica",
+            "speciesSCI":"Kuikka",
+            "speciesAbbr":"Storlom",
+            "speciesGroup_id":null,
+            "visibility":1
+        },
+        {
+            "mxCode":25844,
+            "speciesFI":"TACRUF",
+            "speciesEN":"Tachybaptus ruficollis",
+            "speciesSCI":"Pikku-uikku",
+            "speciesAbbr":"Smådopping",
+            "speciesGroup_id":null,
+            "visibility":1
+        },
+        {
+            "mxCode":25859,
+            "speciesFI":"PODGRI",
+            "speciesEN":"Podiceps grisegena",
+            "speciesSCI":"Härkälintu",
+            "speciesAbbr":"Gråhakedopping",
+            "speciesGroup_id":null,
+            "visibility":1
+        },
+        {
+            "mxCode":25860,
+            "speciesFI":"PODCRI",
+            "speciesEN":"Podiceps cristatus",
+            "speciesSCI":"Silkkiuikku",
+            "speciesAbbr":"Skäggdopping",
+            "speciesGroup_id":null,
+            "visibility":1
+        },
+    ]
+
+    function getAll() {
+        return Promise.resolve(data)
+    }
+
+}
+
+module.exports = BirdDao
