34c7a76c1b407dc8cea093c61756b71b11f5ee5c
Richard Raita <48387692+ric-rai@users.noreply.github.com>
2021-09-29 13:23:13 +0300

    Add coordinate transformations by matrices

--- src/main/domain/map_service.js
+++ src/main/domain/map_service.js
@@ -17,7 +17,7 @@ function MapService() {
             return this
         },
         setViewBox: function (width, height) {
-            this.document.documentElement.setAttribute('viewBox', `0 0 ${width} ${height}`)
+            this.svg.setAttribute('viewBox', `0 -150 ${width} ${height}`)
             return this
         },
         addRectangle: function (id, x, y, width, height, fill) {
@@ -35,6 +35,10 @@ function MapService() {
         },
     }
 
+    const verticalFlipMatrix = [[-1, 0], [0, 1]]
+    const rotate180ccwMatrix = [[-1, 0], [0, -1]]
+    const vFlipRot180ccwMatrix = multiplyMatrices(verticalFlipMatrix, rotate180ccwMatrix)
+
     return {
         "getGridOverlay": (gridArray) => {
             const minEastCoordinate = Math.min(...gridArray.map(grid => grid.e))
@@ -42,10 +46,28 @@ function MapService() {
             const svgGridArray = gridArray.map(grid => ({"id": grid.id,
                 "e": grid.e - minEastCoordinate, "n": grid.n - minNorthCoordinate }))
             svgService.initEmptyDocument(400, 400).setViewBox(400, 400)
-            svgGridArray.forEach(grid => svgService.addRectangle(
-                grid.id, grid.e, grid.n, 1, 1, "black"))
+            svgGridArray.forEach(grid => {
+                const flipped = multiplyMatrices([[grid.e, grid.n]], vFlipRot180ccwMatrix)
+                return svgService.addRectangle(
+                    grid.id, flipped[0][0], flipped[0][1], 1, 1, "black")
+            })
             return svgService.serializeDocument()
         }
+    }
+
+    function multiplyMatrices(m1, m2) {
+        const result = [];
+        for (let i = 0; i < m1.length; i++) {
+            result[i] = [];
+            for (let j = 0; j < m2[0].length; j++) {
+                let sum = 0;
+                for (let k = 0; k < m1[0].length; k++) {
+                    sum += m1[i][k] * m2[k][j];
+                }
+                result[i][j] = sum;
+            }
+        }
+        return result;
     }
 
 }
