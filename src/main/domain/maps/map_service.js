feec1fe7230d7b86d0e27332ea1e992247ee9068
Richard Raita <48387692+ric-rai@users.noreply.github.com>
2021-09-29 17:01:28 +0300

    Make coordinates to start at 0

--- src/main/domain/maps/map_service.js
+++ src/main/domain/maps/map_service.js
@@ -2,24 +2,38 @@ const SvgService = require(__rootdir + "/domain/maps/svg_service.js")
 
 function MapService() {
     const svgService = SvgService()
-    const verticalFlipMatrix = [[-1, 0], [0, 1]]
-    const rotate180ccwMatrix = [[-1, 0], [0, -1]]
-    const vFlipRot180ccwMatrix = multiplyMatrices(verticalFlipMatrix, rotate180ccwMatrix)
 
     return {
         getGridOverlay: (gridArray) => {
-            const minEastCoordinate = Math.min(...gridArray.map(grid => grid.e))
-            const minNorthCoordinate = Math.min(...gridArray.map(grid => grid.n))
+            const verticalFlipMatrix = [[-1, 0], [0, 1]]
+            const rotate180ccwMatrix = [[-1, 0], [0, -1]]
+            const transformationMatrix = multiplyMatrices(verticalFlipMatrix, rotate180ccwMatrix)
+            const minMaxValues = transformCoordsByMatrix(gridArray, transformationMatrix)
             const svgGridArray = gridArray.map(grid => ({"id": grid.id,
-                "e": grid.e - minEastCoordinate, "n": grid.n - minNorthCoordinate }))
-            svgService.initEmptyDocument(400, 400).setViewBox(400, 400)
-            svgGridArray.forEach(grid => {
-                const flipped = multiplyMatrices([[grid.e, grid.n]], vFlipRot180ccwMatrix)
-                return svgService.addRectangle(
-                    grid.id, flipped[0][0], flipped[0][1], 1, 1, "black")
-            })
+                "e": grid.e - minMaxValues.minX, "n": grid.n - minMaxValues.minY }))
+            const width = Math.abs(minMaxValues.maxX - minMaxValues.minX)
+            const height = Math.abs(minMaxValues.maxY - minMaxValues.minY)
+            svgService.initEmptyDocument(width, height).setViewBox(width, height)
+            svgGridArray.forEach(grid => svgService.addRectangle(
+                    grid.id, grid.e, grid.n, 1, 1, "black"))
             return svgService.serializeDocument()
         }
+    }
+
+    function transformCoordsByMatrix(coordArray, matrix) {
+        let [minX, minY] = [Number.MAX_SAFE_INTEGER, Number.MAX_SAFE_INTEGER]
+        let [maxX, maxY] = [0, 0]
+        for (let i = 0; i < coordArray.length; i++) {
+            const coordMatrix = [[coordArray[i].e, coordArray[i].n]]
+            const transformedCoords = multiplyMatrices(coordMatrix, matrix)
+            coordArray[i].e = transformedCoords[0][0]
+            coordArray[i].n = transformedCoords[0][1]
+            if (coordArray[i].e < minX) minX = coordArray[i].e
+            if (coordArray[i].n < minY) minY = coordArray[i].n
+            if (coordArray[i].e > maxX) maxX = coordArray[i].e
+            if (coordArray[i].n > maxY) maxY = coordArray[i].n
+        }
+        return {minX, minY, maxX, maxY}
     }
 
     function multiplyMatrices(m1, m2) {
