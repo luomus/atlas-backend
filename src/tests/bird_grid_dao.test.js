52b032976281065fe6b474935904e172e1a66acd
Emma Valtonen <emma.valtonen@helsinki.fi>
2021-09-30 11:33:45 +0300

    Fix bird grid dao tests

--- src/tests/bird_grid_dao.test.js
+++ src/tests/bird_grid_dao.test.js
@@ -7,22 +7,24 @@ beforeEach(() => {
     querier.mockClear()
 })
 
-test('getById calls querier correctly', () => {
+test('getGridByIdAtlas3 calls querier correctly', () => {
     const birdGridDao = new BirdGridDao(querier)
-    birdGridDao.getById(1)
-    birdGridDao.getById(15)
+    birdGridDao.getGridByIdAtlas3(1)
+    birdGridDao.getGridByIdAtlas3(2)
 
     expect(querier).toHaveBeenCalledTimes(2)
     expect(querier.mock.calls[0][0]).toEqual('get')
+    expect(querier.mock.calls[0][1]).toContain('atlas3')
     expect(querier.mock.calls[0][2]).toEqual([1])
     expect(querier.mock.calls[1][0]).toEqual('get')
-    expect(querier.mock.calls[1][2]).toEqual([15])
+    expect(querier.mock.calls[1][1]).toContain('atlas3')
+    expect(querier.mock.calls[1][2]).toEqual([2])
 })
 
-test('getAll calls querier correctly', () => {
+test('getAllGridsAtlas3 calls querier correctly', () => {
     const birdGridDao = new BirdGridDao(querier)
-    birdGridDao.getAll()
+    birdGridDao.getAllGridsAtlas3()
     expect(querier).toHaveBeenCalledTimes(1)
     expect(querier.mock.calls[0][0]).toEqual('all')
-    expect(querier.mock.calls[0][1]).toContain('SELECT * FROM')
+    expect(querier.mock.calls[0][1]).toContain('atlas3')
 })
