8ddcf3ff267b8d95117f4cc22423080be0645d78
Emma Valtonen <emma.valtonen@helsinki.fi>
2021-09-30 08:04:39 +0300

    Update server test

--- src/tests/server.test.js
+++ src/tests/server.test.js
@@ -1,10 +1,26 @@
+const request = require('supertest')
 const app = require('../main/server')
-const supertest = require('supertest')
-const request = supertest(app)
+jest.mock('../main/dao/bird_dao')
 
-test('', async () => {
-    await request
-        .get('/api/birds')
-        .expect(200)
-        // .expect('content-type', /application\/json/)
+describe('Bird API', () => {
+
+    test('GET /api/birds responds with JSON', () => {
+        return request(app)
+            .get('/api/birds')
+            .expect(200)
+            // .expect('content-type', /application\/json/)
+    })
+
+    test('GET /api/birds responds with data of all birds', () => {
+        return request(app)
+            .get('/api/birds')
+            .then((response) => {
+                expect(response.text).toContain('Kaakkuri')
+                expect(response.text).toContain('Kuikka')
+                expect(response.text).toContain('Pikku-uikku')
+                expect(response.text).toContain('Härkälintu')
+                expect(response.text).toContain('Silkkiuikku')
+            })
+    })
+
 })
