80af3abd51cc098623e53513a0588528057407c7
Emma Valtonen <emma.valtonen@helsinki.fi>
2021-09-30 11:12:03 +0300

    Create mock bird grid dao

---
+++ src/main/dao/__mocks__/bird_grid_dao.js
@@ -0,0 +1,50 @@
+class BirdGridDao {
+    #data
+
+    constructor() {
+        this.#data = [
+            {
+                "id":661312,
+                "coordinateN":661,
+                "coordinateE":312,
+                "municipality_id":147,
+                "gridName":"Bogskär"
+            },
+            {
+                "id":663318,
+                "coordinateN":663,
+                "coordinateE":318,
+                "municipality_id":181,
+                "gridName":"Alu"
+            },
+            {
+                "id":663319,
+                "coordinateN":663,
+                "coordinateE":319,
+                "municipality_id":181,
+                "gridName":"Kalkskär"
+            },
+            {
+                "id":663320,
+                "coordinateN":663,
+                "coordinateE":320,
+                "municipality_id":181,
+                "gridName":"Flutun"
+            },
+            {
+                "id":663321,
+                "coordinateN":663,
+                "coordinateE":321,
+                "municipality_id":181,
+                "gridName":"Ståtbådarna"
+            }
+        ]
+    }
+
+    getAll() {
+        return Promise.resolve(this.#data)
+    }
+
+}
+
+module.exports = BirdGridDao
