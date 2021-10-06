117ae3e45539c80b4710c6a42fa0073c768032a1
juliapalorinne <julia.palorinne@helsinki.fi>
2021-10-06 11:42:21 +0300

    tests to gridDao

--- src/tests/grid_dao.test.js
+++ src/tests/grid_dao.test.js
@@ -23,3 +23,20 @@ test('getAllGrids calls querier correctly', () => {
     expect(querier.mock.calls[0][0]).toEqual('all')
     expect(querier.mock.calls[0][1]).toContain('SELECT * FROM')
 })
+
+test('getMunicipalityById calls querier correctly', () => {
+    const gridDao = new GridDao(querier)
+    gridDao.getMunicipalityById(39)
+
+    expect(querier).toHaveBeenCalledTimes(1)
+    expect(querier.mock.calls[0][0]).toEqual('get')
+    expect(querier.mock.calls[0][2]).toEqual([39])
+})
+
+test('getAllMunicipalities calls querier correctly', () => {
+    const gridDao = new GridDao(querier)
+    gridDao.getAllMunicipalities()
+    expect(querier).toHaveBeenCalledTimes(1)
+    expect(querier.mock.calls[0][0]).toEqual('all')
+    expect(querier.mock.calls[0][1]).toContain('SELECT * FROM')
+})
