9d00355f894f144c9800da58225ee5e9fe0fb173
Mia M <36441227+miamik@users.noreply.github.com>
2021-10-18 09:24:57 +0300

    CheckConnection.js for connecting to Oracle db

---
+++ src/main/checkConnection.js
@@ -0,0 +1,43 @@
+const oracledb = require('oracledb')
+
+async function checkConnection() {
+    let connection;
+    try {
+      connection = await oracledb.getConnection( {
+      user : "atlas_staging",
+      password : "${{secret.ORACLEPASS}}",
+      connectString : "oracle.luomus.fi:1521/oracle.luomus.fi"
+      });
+      console.log("Connected to Oracle Database")
+      
+   
+      let result = await connection.execute("SELECT * FROM species", [], { resultSet: true, outFormat: oracledb.OUT_FORMAT_OBJECT});
+      const rs = result.resultSet;
+      
+      let row;
+
+      while ((row = await rs.getRow())) {
+        if (row.VISIBILITY === 1 && row.MXCODE > 40000) {
+          console.log(row.MXCODE, row.SPECIESFI)
+        }
+      }
+      
+    await rs.close();
+  
+  } catch (err) {
+    console.error(err.message);
+
+    } finally {
+      if (connection) {
+        try {
+          await connection.close();
+          console.log("Database connection closed")
+        } catch (err) {
+          console.error(err.message);
+        }
+      }
+  
+     }
+    }
+   
+  checkConnection();
