43a31c8deb0e00d67b0e5a0a328fb7655bc4858a
juliapalorinne <julia.palorinne@helsinki.fi>
2021-11-10 12:57:13 +0200

    bird dao documentation

--- src/main/dao/bird_dao.js
+++ src/main/dao/bird_dao.js
@@ -1,10 +1,23 @@
-class BirdDao {
+/**
+ * Provides methods for accessing bird species database.
+ */
+ class BirdDao {
     #querier
 
+    
+    /**
+     * @constructor
+     * @param {Querier} querier 
+     */
     constructor(querier) {
         this.#querier = querier
     }
-    
+
+
+    /**
+     * Creates table Species if that does not exist.
+     * @returns {Promise}
+     */
     createTable() {
         const sql = `CREATE TABLE IF NOT EXISTS species (
             id INTEGER AUTOINCREMENT,
@@ -19,13 +32,26 @@ class BirdDao {
         return this.#querier('run', sql)
     }
 
-    create(mxcode, abbr, nameSCI, nameFI, nameSV, nameEN, group_id) {
+    
+    /**
+     * Creates new entry with given info in table Species.
+     * @param {{mxcode: number, abbr: string, nameSCI: string, nameFI: string, nameSV: string, nameEN: string, group_id: number}} species
+     * @returns {Promise}
+     */
+    create(species) {
+        const { mxcode, abbr, nameSCI, nameFI, nameSV, nameEN, group_id } = species
         const sql = `INSERT INTO species 
             (mxcode, speciesFI, speciesSV, speciesEN, speciesSCI, speciesAbbr, speciesGroup_id) 
             VALUES (?, ?, ?, ?, ?, ?, ?)`
         return this.#querier('run', sql, [mxcode, abbr, nameSCI, nameFI, nameSV, nameEN, group_id])
     }
 
+
+    /**
+     * Updates the entry with given mx-code in table Species.
+     * @param {{mxcode: number, abbr: string, nameSCI: string, nameFI: string, nameSV: string, nameEN: string}} species
+     * @returns {Promise}
+     */
     update(species) {
         const { mxcode, abbr, nameSCI, nameFI, nameSV, nameEN } = species
         const sql = `UPDATE species
@@ -38,18 +64,41 @@ class BirdDao {
         return this.#querier('run', sql, [abbr, nameSCI, nameFI, nameSV, nameEN, mxcode])
     }
 
+
+    /**
+     * Deletes the entry with given mx-code from table Species.
+     * @param {number} mxcode 
+     * @returns {Promise}
+     */
     delete(mxcode) {
         return this.#querier('run', `DELETE FROM species WHERE mxcode = ?`, [mxcode])
     }
 
+
+    /**
+     * Returns the database search result from table Species with given mx-code.
+     * @param {number} mxcode 
+     * @returns {Promise}
+     */
     getById(mxcode) {
         return this.#querier('get', `SELECT * FROM species WHERE mxcode = ?`, [mxcode])
     }
 
+
+    /**
+     * Returns the database search result from table Species with given species group id.
+     * @param {number} speciesGroup_id
+     * @returns {Promise}
+     */
     getAllByGroup(speciesGroup_id) {
         return this.#querier('get', `SELECT * FROM species WHERE speciesGroup_id = ?`, [speciesGroup_id])
     }
 
+
+    /**
+     * Returns the database search result for all data from table Species.
+     * @returns {Promise}
+     */
     getAll() {
         return this.#querier('all', `SELECT * FROM species`)
     }
