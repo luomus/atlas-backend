8b7fa7d34de4b3dab87718bd0eba336efd6e2241
juliapalorinne <julia.palorinne@helsinki.fi>
2021-11-10 12:58:45 +0200

    bird grid dao documentation

--- src/main/dao/bird_grid_dao.js
+++ src/main/dao/bird_grid_dao.js
@@ -1,10 +1,23 @@
-class BirdGridDao {
+/**
+ * Provides methods for accessing Bird Atlas database.
+ */
+ class BirdGridDao {
     #querier
 
+    
+    /**
+     * @constructor
+     * @param {Querier} querier 
+     */
     constructor(querier) {
         this.#querier = querier
     }
 
+
+    /**
+     * Creates table for Atlas3 grids if that does not exist.
+     * @returns {Promise}
+     */
     createTableGridAtlas3() {
         const sql = `CREATE TABLE IF NOT EXISTS grid_atlas3 (
             id INTEGER PRIMARY KEY AUTOINCREMENT,
@@ -19,15 +32,30 @@ class BirdGridDao {
         return this.#querier('run', sql)
     }
 
+
+    /**
+     * Returns the database search result for all Atlas3 grids with given grid id.
+     * @param {number} grid_id
+     * @returns {Promise}
+     */
     getGridByIdAtlas3(grid_id) {
         return this.#querier('get', `SELECT * FROM grid_atlas3 WHERE grid_id = ?`, [grid_id])
     }
 
+
+    /**
+     * Returns the database search result for all Atlas3 grids.
+     * @returns {Promise}
+     */
     getAllGridsAtlas3() {
         return this.#querier('all', `SELECT * FROM grid_atlas3`)
     }
 
 
+    /**
+     * Creates table for Atlas3 bird data if that does not exist.
+     * @returns {Promise}
+     */
     createTableBirdDataAtlas3() {
         const sql = `CREATE TABLE IF NOT EXISTS bird_data_atlas3 (
             id INTEGER PRIMARY KEY AUTOINCREMENT,
@@ -38,25 +66,47 @@ class BirdGridDao {
         return this.#querier('run', sql)
     }
 
+
+    /**
+     * Returns the database search result for all Atlas3 data with given species mx-code.
+     * @param {number} species_mxcode 
+     * @returns {Promise}
+     */
     getBySpeciesFromAtlas3(species_mxcode) {
         return this.#querier('all', `SELECT * FROM bird_data_atlas3 WHERE species_mxcode = ?`, [species_mxcode])
     }
 
+
+    /**
+     * Returns the database search result for all Atlas3 data.
+     * @returns {Promise}
+     */
     getAllDataFromBirdAtlas3() {
         return this.#querier('all', `SELECT * FROM bird_data_atlas3`)
     }
 
+
+    /**
+     * Returns the database search result for all Atlas3 data with given species mx-code with visibility of 1
+     * combined with Finnish name of the species, grid id, grid coordinates, and breeding category.
+     * @param {number} species_mxcode 
+     * @returns {Promise}
+     */
     getGridAndBreedingdataForBird(species_mxcode) {
         return this.#querier('all', `SELECT species.speciesFI, grid.id, grid.coordinateN, grid.coordinateE, bird_data_atlas3.breedingCategory 
-        FROM bird_data_atlas3 JOIN grid 
-        ON grid.id = bird_data_atlas3.grid_id 
-        JOIN species 
-        ON species.mxCode = bird_data_atlas3.species_mxcode 
-        WHERE species.mxcode=? 
-        AND species.visibility=1`, [species_mxcode])
+            FROM bird_data_atlas3 JOIN grid 
+            ON grid.id = bird_data_atlas3.grid_id 
+            JOIN species 
+            ON species.mxCode = bird_data_atlas3.species_mxcode 
+            WHERE species.mxcode=? 
+            AND species.visibility=1`, [species_mxcode])
     }
     
 
+    /**
+     * Creates table for Atlas1-2 grids if that does not exist.
+     * @returns {Promise}
+     */
     createTableGridAtlas12() {
         const sql = `CREATE TABLE IF NOT EXISTS CREATE TABLE grid_atlas12 (
             id INTEGER PRIMARY KEY AUTOINCREMENT,
@@ -67,15 +117,30 @@ class BirdGridDao {
         return this.#querier('run', sql)
     }
 
+
+    /**
+     * Returns the database search result for all Atlas1-2 grids with given grid id.
+     * @param {number} grid_id
+     * @returns {Promise}
+     */
     getGridByIdAtlas12(grid_id) {
         return this.#querier('get', `SELECT * FROM grid_atlas12 WHERE grid_id = ?`, [grid_id])
     }
 
+
+    /**
+     * Returns the database search result for all Atlas1-2 grids.
+     * @returns {Promise}
+     */
     getAllGridsAtlas12() {
         return this.#querier('all', `SELECT * FROM grid_atlas12`)
     }
 
 
+    /**
+     * Creates table for Atlas1-2 bird data if that does not exist.
+     * @returns {Promise}
+     */
     createTableBirdDataAtlas12() {
         const sql = `CREATE TABLE IF NOT EXISTS bird_data_atlas12 (
             id INTEGER PRIMARY KEY AUTOINCREMENT,
@@ -87,10 +152,21 @@ class BirdGridDao {
         return this.#querier('run', sql)
     }
 
+
+    /**
+     * Returns the database search result for all Atlas1-2 data with given species mx-code.
+     * @param {number} species_mxcode 
+     * @returns {Promise}
+     */
     getBySpeciesFromAtlas12(species_mxcode) {
         return this.#querier('all', `SELECT * FROM bird_data_atlas12 WHERE species_mxcode = ?`, [species_mxcode])
     }
 
+
+    /**
+     * Returns the database search result for all Atlas1-2 data.
+     * @returns {Promise}
+     */
     getAllDataFromBirdAtlas12() {
         return this.#querier('all', `SELECT * FROM bird_data_atlas12`)
     }
