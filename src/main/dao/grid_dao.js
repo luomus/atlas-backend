718bb838e760590e416a6347641566b3da5e28e5
juliapalorinne <julia.palorinne@helsinki.fi>
2021-11-10 12:57:56 +0200

    grid dao documentation

--- src/main/dao/grid_dao.js
+++ src/main/dao/grid_dao.js
@@ -1,11 +1,23 @@
-class GridDao {
-
+/**
+ * Provides methods for accessing grid and municipality databases.
+ */
+ class GridDao {
     #querier
 
+    
+    /**
+     * @constructor
+     * @param {Querier} querier 
+     */
     constructor(querier) {
         this.#querier = querier
     }
 
+
+    /**
+     * Creates table Municipality if that does not exist.
+     * @returns {Promise}
+     */
     createTableMunicipality() {
         const sql = `CREATE TABLE IF NOT EXISTS municipality (
             id INTEGER PRIMARY KEY AUTOINCREMENT,
@@ -16,6 +28,11 @@ class GridDao {
         return this.#querier('run', sql)
     }
 
+
+    /**
+     * Creates table Grid if that does not exist.
+     * @returns {Promise}
+     */
     createTableGrid() {
         const sql = `CREATE TABLE IF NOT EXISTS grid (
             id INTEGER PRIMARY KEY AUTOINCREMENT,
@@ -26,6 +43,11 @@ class GridDao {
         return this.#querier('run', sql)
     }
 
+    /**
+     * Updates in table Municipality the entry with given id.
+     * @param {{id: number, name: string, societyNameFI: string, societyNameSV: string, regionNumber: number}} municipality 
+     * @returns {Promise}
+     */
     updateMunicipality(municipality) {
         const { id, name, societyNameFI, societyNameSV, regionNumber } = municipality
         const sql = `UPDATE municipality
@@ -37,6 +59,12 @@ class GridDao {
         return this.#querier('run', sql, [name, societyNameFI, societyNameSV, regionNumber, id])
     }
 
+
+    /**
+     * Updates in table Grid the entry with given id.
+     * @param {{id: number, coordinateN: number, coordinateE: number, municipality_id: number, gridName: string}} grid 
+     * @returns {Promise}
+     */
     updateGrid(grid) {
         const { id, coordinateN, coordinateE, municipality_id, gridName } = grid
         const sql = `UPDATE grid
@@ -48,26 +76,60 @@ class GridDao {
         return this.#querier('run', sql, [coordinateN, coordinateE, municipality_id, gridName, id])
     }
 
+
+    /**
+     * Deletes from table Municipality the entry with given id.
+     * @param {number} id 
+     * @returns {Promise}
+     */
     deleteMunicipality(id) {
         return this.#querier('run', `DELETE FROM municipality WHERE id = ?`, [id])
     }
 
+
+    /**
+     * Deletes from table Grid the entry with given id.
+     * @param {number} id 
+     * @returns {Promise}
+     */
     deleteGrid(id) {
         return this.#querier('run', `DELETE FROM grid WHERE id = ?`, [id])
     }
 
+
+    /**
+     * Returns the database search result from table Municipality with given id.
+     * @param {number} id 
+     * @returns {Promise}
+     */
     getMunicipalityById(id) {
         return this.#querier('get', `SELECT * FROM municipality WHERE id = ?`, [id])
     }
 
+
+    /**
+     * Returns the database search result from table Grid with given id.
+     * @param {number} id 
+     * @returns {Promise}
+     */
     getGridById(id) {
         return this.#querier('get', `SELECT * FROM grid WHERE id = ?`, [id])
     }
 
+
+    /**
+     * Returns the database search result for all data from table Municipality.
+     * @returns {Promise}
+     */
     getAllMunicipalities() {
         return this.#querier('all', `SELECT * FROM municipality`)
     }
 
+
+    /**
+     * Returns the database search result for all data from table Grid.
+     * @returns {Promise}
+     */
     getAllGrids() {
         return this.#querier('all', `SELECT * FROM grid`)
     }
