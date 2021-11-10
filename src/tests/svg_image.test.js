b56446b4d357da92903544c73a1097597d68f1b2
Emma Valtonen <emma.valtonen@helsinki.fi>
2021-11-10 19:23:27 +0200

    Fix svg image tests

--- src/tests/svg_image.test.js
+++ src/tests/svg_image.test.js
@@ -8,23 +8,23 @@ beforeEach(() => {
     svgImage.setDimensions(10, 20)
 })
 
-test('An empty svg image is created', () => new SvgImage())
+test('An empty svg image is created', () => {
+    const svg = new SvgImage()
+    expect(svg.serialize()).toEqual('<svg xmlns="http://www.w3.org/2000/svg"/>')
+})
 
-test('initEmptyDocument creates svg with correct width & height', () => {
+test('setDimensions sets correct width', () => {
     svgImage.setDimensions(200, 300)
-    const svg1 = svgImage.serialize()
-    svgImage.setDimensions(10, 10)
-    const svg2 = svgImage.serialize()
+    const svg = svgImage.serialize()
+    expect(svg).toMatch(/^<svg.+\/>$/)
+    expect(svg).toContain('width="200"')
+})
 
-    expect(svg1).toMatch(/^<svg.+\/>$/)
-    expect(svg1).toContain('width="200"')
-    expect(svg1).toContain('height="300"')
-    expect(svg1).toContain('xmlns="http://www.w3.org/2000/svg"')
-
-    expect(svg2).toMatch(/^<svg.+\/>$/)
-    expect(svg2).toContain('width="10"')
-    expect(svg2).toContain('height="10"')
-    expect(svg2).toContain('xmlns="http://www.w3.org/2000/svg"')
+test('setDimensions sets correct height', () => {
+    svgImage.setDimensions(200, 300)
+    const svg = svgImage.serialize()
+    expect(svg).toMatch(/^<svg.+\/>$/)
+    expect(svg).toContain('height="300"')
 })
 
 test('setViewBox sets correct attributes', () => {
@@ -40,11 +40,11 @@ test('setViewBox sets correct attributes', () => {
     expect(svg2).toContain('viewBox="1 2 3 4"')
 })
 
-test('addCircle adds circle element with correct attributes', () => {
+test('addElement adds element with correct attributes', () => {
     const propertyMap1 = {id: 1, cx: 1, cy: 1, fill: "black", r: 0.5}
     const propertyMap2 = {id: 2, cx: 5, cy: 10, fill: "red", r: 1}
-    svgImage.addCircle(propertyMap1)
-    svgImage.addCircle(propertyMap2)
+    svgImage.addElement('circle', propertyMap1)
+    svgImage.addElement('circle', propertyMap2)
     const svg = svgImage.serialize()
 
     expect(svg).toMatch(/^<svg.+\>.+<\/svg>$/)
@@ -52,18 +52,8 @@ test('addCircle adds circle element with correct attributes', () => {
     expect(svg).toContain('<circle id="2" cx="5" cy="10" fill="red" r="1"/>')
 })
 
-test('addCircle adds no extra attributes to circle', () => {
-    const propertyMap = {testProperty: 1}
-    svgImage.addCircle(propertyMap)
-    const svg = svgImage.serialize()
-
-    expect(svg).toMatch(/^<svg.+\>.+<\/svg>$/)
-    expect(svg).toContain('<circle testProperty="1"/>')
-    expect(svg).not.toContain('fill')
-})
-
 test('copy works correctly', () => {
-    svgImage.addCircle({testProperty: 1})
+    svgImage.addElement('circle', {testProperty: 1})
     const copy = svgImage.copy()
     copy.setDimensions(1, 1)
     const svg = copy.serialize()
@@ -73,26 +63,40 @@ test('copy works correctly', () => {
     expect(svg).not.toContain('height="20"')
 })
 
-test('changeDisplayForAll changes display to block', () => {
-    svgImage.addCircle({testProperty: 1})
-    svgImage.changeDisplayForAll(display = true)
-    const svg = svgImage.serialize()
-
-    expect(svg).toContain('display=\"block\"')
-})
-
-test('changeDisplayForAll changes display to none', () => {
-    svgImage.addCircle({testProperty: 1})
-    svgImage.changeDisplayForAll(display = false)
-    const svg = svgImage.serialize()
-
-    expect(svg).toContain('display=\"none\"')
-})
-
-test('setAtrribute changes the color of a circle', () => {
-    svgImage.addCircle({testProperty: 1, id: 1})
-    svgImage.setAttribute(1, {fill: 'red'})
+test('setAttributesOfElement sets a new attribute', () => {
+    svgImage.addElement('circle', {testProperty: 1, id: 1})
+    svgImage.setAttributesOfElement(1, {fill: 'red'})
     const svg = svgImage.serialize()
 
     expect(svg).toContain('fill=\"red\"')
+})
+
+test('setAttributesOfElement updates an existing attribute', () => {
+    svgImage.addElement('circle', {id: 1, testProperty: 1, fill: 'red'})
+    svgImage.setAttributesOfElement(1, {fill: 'black'})
+    const svg = svgImage.serialize()
+
+    expect(svg).not.toContain('fill=\"red\"')
+    expect(svg).toContain('fill=\"black\"')
+})
+
+test('setAttributesForAllElements sets a new attribute for all elements', () => {
+    svgImage.addElement('circle', {id: 1, class: 'testClass'})
+    svgImage.addElement('circle', {id: 2, class: 'testClass'})
+    svgImage.setAttributesForAllElements('testClass', {fill: 'red'})
+    const svg = svgImage.serialize()
+    
+    expect(svg).toContain('id="1" class="testClass" fill="red"')
+    expect(svg).toContain('id="2" class="testClass" fill="red"')
+})
+
+test('setAttributesForAllElements updates an existing attribute for all elements', () => {
+    svgImage.addElement('circle', {id: 1, class: 'testClass', display: 'block'})
+    svgImage.addElement('circle', {id: 2, class: 'testClass', display: 'block'})
+    svgImage.setAttributesForAllElements('testClass', {display: 'none'})
+    const svg = svgImage.serialize()
+
+    expect(svg).not.toContain('display="block')
+    expect(svg).toContain('id="1" class="testClass" display="none')
+    expect(svg).toContain('id="2" class="testClass" display="none')
 })
